933e974a8aab5740cab324ed46a0c0e16b6cfdd5
Pritam688Pal <pritampal688@gmail.com>
2026-06-19 13:19:42 +0530

    fix: update routing to navigate to LandingPage for unmatched routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 // import './App.css';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Routes, Navigate } from 'react-router-dom';
 import LandingPage from './pages/LandingPage.jsx';
 import Authentication from './pages/Authentication.jsx';
 import HomeComponent from './pages/HomePage.jsx'
@@ -12,12 +12,14 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
+          <Route path="/" element={<LandingPage />} />
           <Route path="/auth/:state" element={<Authentication />} />
           <Route path="/home" element={<HomeComponent />} />
           <Route path='/:url' element={<VideoMeet />} />
           <Route path='/history' element={<History />} />
-          <Route path="*" element={<LandingPage />} />
 
+
+          <Route path="*" element={<Navigate to="/" replace/>} />
         </Routes>
       </Router>
     </div>
